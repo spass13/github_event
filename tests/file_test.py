503e5e1faa0d865a36c39c4ab0674880166c217b
spass13 <alex-strack@t-online.de>
2025-06-01 14:52:57 +0200

    Update file_test.py

--- tests/file_test.py
+++ tests/file_test.py
@@ -5,17 +5,21 @@ def test_calc_addition():
     output = 2 + 4
     assert output == 6
 
+
 # If you need further assistance, feel free to ask!
+
 
 def test_calc_subtraction():
     # Function to test the output of 2 - 4
     output = 2 - 4
     assert output == -2
 
+
 def test_calc_multiply():
     # Function to test the output of 2*4
     output = 2 * 4
     assert output == 8
+
 
 def test_coucou():
     # Function to test if the output returns 'hello'
